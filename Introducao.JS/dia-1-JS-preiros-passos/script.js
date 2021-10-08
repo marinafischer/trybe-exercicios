0e6b52fc5328461a2a2aa2708d52fa2599e2719f
Marina Fischer <marinamirandafischer@gmail.com>
2021-10-08 15:02:15 -0300

    criando branch dia 4.1

---
+++ Introducao.JS/dia-1-JS-preiros-passos/script.js
@@ -0,0 +1,32 @@
+/*
+const myName = 'Marina';
+const birthCity = 'São Leopoldo';
+const birthYear = 1990;
+
+
+console.log(myName, birthCity, birthYear)
+
+
+let patientId = 50;
+let isEnrolled = true;
+const patientInfo = {
+  firstName: 'Ana',
+  lastName: 'Santos',
+};
+const patientEmail = 'ana@email.com';
+
+console.log(typeof patientId)
+console.log(typeof isEnrolled)
+console.log(typeof patientInfo)
+console.log(typeof patientEmail)
+console.log(typeof patientAge)
+
+
+
+const base = 5;
+let altura = 8;
+const area = base*altura;
+const perimeter = 2*base + 2*altura;
+
+console.log(area, perimeter)
+*/
