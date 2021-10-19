5ce60b7df152c797bd1db8558ff088160cffdae3
Marina Fischer <marinamirandafischer@gmail.com>
2021-10-19 18:59:02 -0300

    finalizaçao exercicios

--- JS-dom-eventos-webstorage/dia-1-Dom-seletores/Exemplos/script.js
+++ JS-dom-eventos-webstorage/dia-1-Dom-seletores/Exemplos/script.js
@@ -1,26 +1,26 @@
 let cabecalho = document.getElementById('header-container');
-cabecalho.style.backgroundColor = 'lightgreen';
+cabecalho.style.backgroundColor = 'rgb(0, 176, 105)';
 
 let tarefasUrgentes = document.querySelectorAll('.emergency-tasks');
 for (let tarefa of tarefasUrgentes) {
-  tarefa.style.backgroundColor = 'pink';
+  tarefa.style.backgroundColor = 'rgb(255, 159, 132)';
 }
 
 let tituloTarefa = document.querySelectorAll('h3');
 for (let i=0; i<=1; i++) {
-  tituloTarefa[i].style.backgroundColor = 'purple';
+  tituloTarefa[i].style.backgroundColor = 'rgb(165, 0, 243)';
 }
 
 for (let i=2; i<=3; i++) {
-  tituloTarefa[i].style.backgroundColor = 'black';
+  tituloTarefa[i].style.backgroundColor = 'rgb(35, 37, 37)';
 }
 
 let tarefasNaoUrgentes = document.querySelectorAll('.no-emergency-tasks');
 for (let tarefa of tarefasNaoUrgentes) {
-  tarefa.style.backgroundColor = 'yellow';
+  tarefa.style.backgroundColor = 'rgb(249, 219, 94)';
 }
 
 let rodaPe = document.getElementById('footer-container');
-rodaPe.style.backgroundColor='greens'
+rodaPe.style.backgroundColor='rgb(0, 53, 51)';
 
 
