1b14d63bdccf34e58a199577ca359e082fd8a1b5
Marina Fischer <marinamirandafischer@gmail.com>
2021-10-28 20:50:05 -0300

    metade do 3

--- HTML -CSS:Forms-Flexbox-Responsividade/dia-1-Forms/script.js
+++ HTML -CSS:Forms-Flexbox-Responsividade/dia-1-Forms/script.js
@@ -110,6 +110,20 @@ const estados = [
   },
 ];
 const btnEnviar = document.getElementById('enviarDados');
+const corpoDaPagina = document.querySelector('body');
+let dados = '';
+let erro = '';
+let verificador = true;
+const nome = document.getElementById('nome'); 
+const email = document.getElementById('email');
+const cpf = document.getElementById('cpf'); 
+const endereco = document.getElementById('endereco');
+const cidade = document.getElementById('cidade');
+const estado = document.getElementById('estado');
+const tipoMoradia = document.getElementsByName('tipo-moradia');
+
+
+
 
 for (let i in estados) {
   const estado = document.createElement('option');
@@ -120,4 +134,69 @@ for (let i in estados) {
 
 btnEnviar.addEventListener('click', (e)=>{
   e.preventDefault();
-})
+  verificarInfos();
+  if(verificador === false) {
+    imprimeMensagemErro();
+  } else {
+    imprimeDadosConsolidados();
+  }
+});
+
+function verificarInfos () {
+  if(nome.value.trim() !== '' || nome.length <= 40 ) {
+    dados += `nome: ${nome.value};`
+  } else {
+    verificador = false;
+    erro += ` Nome inválido;`
+  }
+  if(email.value.trim() !== '' || email.length <= 50 ) {
+    dados += ` e-mail: ${email.value};`
+  } else {
+    verificador = false;
+    erro += ` email inválido;`
+  }
+  if(cpf.value.trim() !== '' || cpf.length <= 11 ) {
+    dados += ` e-mail: ${cpf.value};`
+  } else {
+    verificador = false;
+    erro += ` CPF inválido;`
+  }
+  if(endereco.value.trim() !== '' || endereco.length <= 200 ) {
+    dados += ` Endereço: ${endereco.value};`
+  } else {
+    verificador = false;
+    erro += ` Endereço inválido;`
+  }
+  if(cidade.value.trim() !== '' || cidade.length <= 28 ) {
+    dados += ` Cidade: ${cidade.value};`
+  } else {
+    verificador = false;
+    erro += ` Cidade inválido;`
+  }
+  if(estado.value.trim() !== '') {
+    dados += ` estado: ${estado.value};`
+  } else {
+    verificador = false;
+    erro += ` estado inválido;`
+  } 
+  if(tipoMoradia[0].checked == false && tipoMoradia[1].checked == false) {
+    verificador = false;
+    erro += ` selecione um tipo de Moradia;`
+  } else if  (tipoMoradia[0].checked == true) {
+    dados += ` Tipo de Moradia: casa;`
+  } else {
+    dados += ` Tipo de Moradia: ap;`
+  }
+}
+
+function imprimeDadosConsolidados () {
+  const dadosConsolidados = document.createElement('div');
+  dadosConsolidados.innerHTML = dados;
+  corpoDaPagina.appendChild(dadosConsolidados);
+}
+
+function imprimeMensagemErro () {
+  const mensagemErro = document.createElement('div');
+  mensagemErro.innerHTML = erro;
+  corpoDaPagina.appendChild(mensagemErro);
+}
