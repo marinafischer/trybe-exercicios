106c7eac47f1b385714356ea126ea28d0563156f
Marina Fischer <marinamirandafischer@gmail.com>
2021-11-16 19:25:42 -0300

    exercicos 1 e 2

---
+++ Higher-Order-Functions/dia1-introducao-HOF/exercicio2.js
@@ -0,0 +1,19 @@
+//2 - Desenvolva uma HOF que retorna o resultado de um sorteio. Esta HOF irá gerar um número aleatório entre 1 e 5 recebendo como parâmetros o número apostado e uma função que checa se o número apostado é igual ao número sorteado. O retorno da sua HOF deve ser uma string (Ex: "Tente novamente" ou "Parabéns você ganhou").
+
+const resultadoSorteio = (aposta) => {
+  const numeroSorteado = Math.floor(Math.random() * 5 + 1);
+  verificaResultado(aposta, numeroSorteado)? console.log('Parabéns você ganhou') : console.log('Tente novamente')
+}
+
+const verificaResultado = (aposta, numeroSorteado) => aposta === numeroSorteado
+
+// {
+//   console.log(aposta, numeroSorteado)
+//   aposta === numeroSorteado ? true : false
+//   // if (aposta === numeroSorteado) {
+//   //   return true
+//   // }
+//   // return false
+// }
+
+resultadoSorteio(3)
