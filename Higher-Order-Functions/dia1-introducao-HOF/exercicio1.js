2a06df94cf99b0ffe3a97cc488dd87a4ba2c865d
Marina Fischer <marinamirandafischer@gmail.com>
2021-11-17 19:04:20 -0300

    exemplos com fund e forEach

--- Higher-Order-Functions/dia1-introducao-HOF/exercicio1.js
+++ Higher-Order-Functions/dia1-introducao-HOF/exercicio1.js
@@ -1,10 +1,10 @@
 // 1 - Crie uma função que retorne um objeto no formato { nomeCompleto, email } representando uma nova pessoa contratada. Passe sua função como parâmetro da HOF newEmployees para criar cada pessoa contratada em seu respectivo id . A sua função deve receber como parâmetro o nome completo da pessoa funcionária e a partir dele gerar automaticamente um email no formato nome_da_pessoa@trybe.com .
 
-const newEmployees = () => {
+const newEmployees = (funcao) => {
   const employees = {
-    id1: nomeEmail('Pedro Guerra'), 
-    id2: nomeEmail('Luiza Drumond '), 
-    id3: nomeEmail('Carla Paiva'), 
+    id1: funcao('Pedro Guerra'), 
+    id2: funcao('Luiza Drumond '), 
+    id3: funcao('Carla Paiva'), 
   }
   return employees;
 };
@@ -18,4 +18,4 @@ const nomeEmail = (nomeCompleto) => {
   }
 }  
 
-console.log(newEmployees())
+console.log(newEmployees(nomeEmail)['id2'])
--- Higher-Order-Functions/dia2-forEach-find-some-every-sort/forEach.js
+++ Higher-Order-Functions/dia2-forEach-find-some-every-sort/forEach.js
@@ -1,7 +1,30 @@
 //Agora vamos usar o forEach , para realizar a tabuada do 2. 
 
-const numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+// const numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 
-const tabuadaDOis = numbers.forEach((numero)=>{
-  console.log(`${numero} x 2 = ${numero*2}`)
-})
+// const tabuadaDOis = numbers.forEach((numero)=>{
+//   console.log(`${numero} x 2 = ${numero*2}`)
+// })
+
+// const names = ['Bianca', 'Camila', 'Fernando', 'Ana Roberta'];
+
+// const convertToUpperCase = (name, index) => {
+//   names[index] = name.toUpperCase();
+// };
+
+// names.forEach(convertToUpperCase);
+// console.log(names); // [ 'BIANCA', 'CAMILA', 'FERNANDO', 'ANA ROBERTA' ]
+
+//1 - Use o método forEach chamando a callback showEmailList para apresentar os emails
+const emailListInData = [
+  'roberta@email.com',
+  'paulo@email.com',
+  'anaroberta@email.com',
+  'fabiano@email.com',
+];
+
+const showEmailList = (email) => {
+  console.log(`O email ${email} esta cadastrado em nosso banco de dados!`);
+};
+
+emailListInData.forEach(showEmailList)
